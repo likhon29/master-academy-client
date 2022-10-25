d6c13273d2b4917dab1a3a7daf1fbcb160be3300
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-25 16:56:16 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routes } from './routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
-        <RouterProvider router={routes}></RouterProvider>
+      <RouterProvider router={routes}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,42 @@
     import React from 'react';
-    
+    import { CDBFooter, CDBFooterLink, CDBBtn, CDBIcon, CDBContainer,CDBBox } from 'cdbreact';
+    import logo from '../../../assets/images/logo.png'
     const Footer = () => {
         return (
-            <div>
-                <h1>Footer</h1>
-            </div>
+            <CDBFooter className="shadow mt-5 bg-warning">
+      <CDBBox
+        display="flex"
+        justifyContent="between"
+        alignItems="center"
+        className="mx-auto py-4 flex-wrap"
+        style={{ width: '80%' }}
+      >
+        <CDBBox display="flex" alignItems="center">
+          <a href="/" className="d-flex align-items-center p-0 text-dark">
+            <img
+              alt="logo"
+              src={logo}
+              width="120px"
+            />
+            <span className="ms-2 ml-4 h5 mb-0 font-weight-bold">Master Academy</span>
+          </a>
+        </CDBBox>
+        <CDBBox>
+          <small className="ml-2">&copy; Master Academy, 2022. All rights reserved.</small>
+        </CDBBox>
+        <CDBBox display="flex">
+          <CDBBtn flat color="dark" className="p-2">
+            <CDBIcon fab icon="facebook-f" />
+          </CDBBtn>
+          <CDBBtn flat color="dark" className="mx-3 p-2">
+            <CDBIcon fab icon="twitter" />
+          </CDBBtn>
+          <CDBBtn flat color="dark" className="p-2">
+            <CDBIcon fab icon="instagram" />
+          </CDBBtn>
+        </CDBBox>
+      </CDBBox>
+    </CDBFooter>
         );
     };
     
