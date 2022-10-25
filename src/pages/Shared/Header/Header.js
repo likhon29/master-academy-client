09924c0bb8312738e47506d972b59a898ed060cc
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-25 13:50:31 +0600

    Navbar added

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -14,7 +14,77 @@ import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 const Header = () => {
     return (
         <div className=" bg-secondary ">
-      
+      {["lg"].map((expand) => (
+  <Navbar key={expand} expand={expand} className=" container mb-3 ">
+    <Container fluid>
+      <Navbar.Brand className=" text-white" as={Link} to="/">
+        <img src={logo} width="200px" height="100px" alt="" />{" "}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+      <Navbar.Offcanvas
+        id={`offcanvasNavbar-expand-${expand}`}
+        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          {/* <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+            Master Academy
+          </Offcanvas.Title> */}
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          
+          <Nav className=" justify-content-end flex-grow-1 px-5 fs-5">
+            <Nav.Link className=" me-5 " as={Link} to="/">
+              Home
+            </Nav.Link>
+            {/* <Nav.Link className="text-white me-5"  as={Link} to="/news">News</Nav.Link> */}
+            <Nav.Link
+              className=" me-5"
+              as={Link}
+              to="/courses"
+            >
+              Courses
+            </Nav.Link>
+            <Nav.Link className=" me-5" as={Link} to="/faq">
+              FAQ
+            </Nav.Link>
+            <Nav.Link className=" me-5" as={Link} to="/blogs">
+              Blog
+            </Nav.Link>
+               <Nav.Link className=" text-white btn btn-primary px-5" href="/login">Login</Nav.Link>
+          
+            <Nav.Link className="text-white  me-5">
+              {/* {user?.uid ? (
+                <>
+                  <span className="text-">{user?.displayName}</span>
+
+                  <Button
+                    className="ms-2"
+                    variant="danger"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Nav.Link
+                    className=" text-white btn btn-primary pt-0 px-5"
+                    as={Link}
+                    to="/login"
+                  >
+                    Login
+                  </Nav.Link>
+                </>
+              )} */}
+            </Nav.Link>
+          </Nav>
+         
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+    </Container>
+  </Navbar>
+))}
     </div>
     );
 };
