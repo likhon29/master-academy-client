b7cc2af03e1ee5b2d84a43b8151e9fad8b5b6a6b
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-27 13:33:20 +0600

    added environment variable to hide firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYi4bkYYcDKimw0yf4WCVtkpopd-YLp_s",
-  authDomain: "master-academy-168b2.firebaseapp.com",
-  projectId: "master-academy-168b2",
-  storageBucket: "master-academy-168b2.appspot.com",
-  messagingSenderId: "605651097061",
-  appId: "1:605651097061:web:c881d78baa4133ed2acb0c",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+
+
+  
 };
 
 // Initialize Firebase
