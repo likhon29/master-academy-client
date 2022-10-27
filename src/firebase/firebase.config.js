d9e3560a48464a37302e9c0d95ffd8fd7ce1c838
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-27 23:48:40 +0600

    firebase key hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -20,9 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export default app;
 
-// apiKey: process.env.REACT_APP_apiKey,
-//     authDomain: process.env.REACT_APP_authDomain,
-//     projectId: process.env.REACT_APP_projectId,
-//     storageBucket: process.env.REACT_APP_storageBucket,
-//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//     appId: process.env.REACT_APP_appId,
+
